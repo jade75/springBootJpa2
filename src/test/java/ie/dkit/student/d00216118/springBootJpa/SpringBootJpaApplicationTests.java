2cc90ef9e00d625e89be82fd95d1234693e2c0e5
jade75 <yu.chen723@gmail.com>
2021-03-13 12:28:06 +0800

    Initial commit

---
+++ src/test/java/ie/dkit/student/d00216118/springBootJpa/SpringBootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.dkit.student.d00216118.springBootJpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
